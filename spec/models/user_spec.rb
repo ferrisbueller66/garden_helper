d8a76096a8d981abf26443027f3ffd12e06c55ba
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-04 21:49:13 -0500

    add unit test user model to confirm has_many through associations work

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,17 +34,27 @@ describe User do
 	end
 
 	  it "has many plants" do
-    first_plant = Plant.create(:user_id => user.id, :bed_id => bed1.id, :variety => "Tomato",)
-    second_plant = Plant.create(:user_id => user.id, :bed_id => bed2.id, :variety => "Thai Basil",)
+    first_plant = Plant.create(:user_id => user.id, :bed_id => bed1.id, :variety => "Tomato")
+    second_plant = Plant.create(:user_id => user.id, :bed_id => bed2.id, :variety => "Thai Basil")
     expect(user.plants.first).to eq(first_plant)
     expect(user.plants.last).to eq(second_plant)
   end
 
-  # it "has many beds through plants" do
-  #   user.beds << [bed1, bed2]
-  #   expect(user.beds.first).to eq(bed1)
-  #   expect(user.bess.last).to eq(bed2)
-  # end
+	it "has many beds through plants" do
+		first_plant = Plant.create(:user_id => user.id, :bed_id => bed1.id, :variety => "Tomato")
+    second_plant = Plant.create(:user_id => user.id, :bed_id => bed2.id, :variety => "Thai Basil")
+    expect(user.beds.first).to eq(bed1)
+    expect(user.beds.last).to eq(bed2)
+	end
+	
+	it "has many harvests through plants" do
+		first_plant = Plant.create(:user_id => user.id, :bed_id => bed1.id, :variety => "Tomato")
+    second_plant = Plant.create(:user_id => user.id, :bed_id => bed2.id, :variety => "Thai Basil")
+		first_harvest = Harvest.create(:weight => "1.2", :harvest_date => 20201201, :plant_id => first_plant.id)
+		second_harvest = Harvest.create(:weight => "2.0", :harvest_date => 20201201, :plant_id => second_plant.id)
+    expect(user.harvests.first).to eq(first_harvest)
+    expect(user.harvests.last).to eq(second_harvest)
+  end
 
 #   it "has a method 'mood' that returns 'sad' when the user is more nauseous than happy" do
 #     expect(user.mood).to eq("sad")
